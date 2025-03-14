ba0484b2ff86ebb6512eacb8028e6a83e54737d0
AndersonWarchon <andersonwarchon2@gmail.com>
2025-03-14 04:53:01 -0300

    primeiro commit

---
+++ pares.py
@@ -0,0 +1,7 @@
+num_escolhido = int(input('Qual número voce deseja saber se é um par ou ímpar?'))
+
+if (num_escolhido % 2 == 0):
+    print("O número", num_escolhido, "É um número par!") 
+else:
+    print("O número", num_escolhido, "É um número ímpar!") 
+    
